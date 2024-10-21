c4e23fc95c357b941b741ceddcde76cd61845818
Lamont Banks <lamontbanks@gmail.com>
2024-10-21 12:39:57 -0400

    Initial commit

---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Start the webserver
+# Go to: http://localhost:8888/
+python3 src/main.py
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    dummy_node = TextNode("this is my name", "italic", "https://www.boot.dev")
+    print(dummy_node)
+
+main()
