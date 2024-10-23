e1787d10de302c3101d6acc813e24fe7a677012d
Lamont Banks <lamontbanks@gmail.com>
2024-10-23 11:10:24 -0400

    More ParentNode tests

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -5,7 +5,10 @@ from leafnode import LeafNode
 
 class TestParentNode(unittest.TestCase):
 
-    # ParentNode with mixed LeafNodes (tag, no tag, props, no props), nested ParentNodes
+    # "Full" test" LeafNodes (tag, no tag, props, no props), nested ParentNodes
+    # Nest ParentNodes - 3 levels
+    # Multiple same-level ParentNodes
+    # ParentNode + 3 leafnodes
     def test_parent_node_full(self):
         # Remove char limit in diff shown failed assertions
         self.maxDiff = None
@@ -66,14 +69,32 @@ class TestParentNode(unittest.TestCase):
         self.assertEqual(node.to_html(), '<div><b>Bold text</b><p style="margin: 15px; line-height: 1.5; text-align: center">Normal text</p><div><a href="http://www.example.com" target="_blank">My link</a>ISO Grocery List<ul><li>French Bread</li><li><b>Celery</b></li><li>Milk</li></ul>Remember, no <i><b>branded</b></i> products!</div></div>')
 
     # ParentNode - Empty children ("no" children)
-
-    # Nest ParentNodes - 3 levels
-
+    def test_parent_node_no_children(self):
+        parent_node = ParentNode(tag="div", children = [])
+        self.assertEqual(parent_node.to_html(), '<div></div>')
+    
     # ParentNode + 1 leafnode
-    # ParentNode + 3 leafnodes
+    def test_parent_node_single_leaf_node(self):
+        parent_node = ParentNode(tag="p", children=[
+            LeafNode(tag="a", props={"href": 'http://www.example.com'}, 
+                     value="Link to example.com")
+        ])
 
-    # Multiple same-level ParentNodes
+        self.assertEqual(parent_node.to_html(), '<p><a href="http://www.example.com">Link to example.com</a></p>')
 
     # Error: ParentNode with None children
+    def test_parent_node_none_children(self):
+        parent_node = ParentNode(tag="div", children=[])
+        parent_node.children = None
 
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+    # Error: ParentNode with no tag
+    def test_parent_with_no_tag(self):
+        parent_node = ParentNode(tag="div", children=[])
+        parent_node.tag = None
+
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
 
